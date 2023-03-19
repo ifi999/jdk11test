72d2d573fe361b2ba04c41adf88a940bdf95756e
ifi999 <kkwkjm4231@gmail.com>
2023-03-19 12:21:48 +0900

    자바 객체 지향의 원리와 이해 - 자바가 확장한 객체 지향 실습

--- src/main/java/etc/OOPSample.java
+++ src/main/java/etc/OOPSample.java
@@ -16,6 +16,29 @@ public class OOPSample {
         p2.name = "P2";
 
         p2.showName();
+
+//        Plant plant = new Plant();
+
+        System.out.println("==================");
+        동물 동물객체 = new 동물();
+        동물 조류객체 = new 조류();
+        동물 참새객체 = new 참새();
+
+        System.out.println(동물객체 instanceof 동물);
+
+        System.out.println(조류객체 instanceof 동물);
+        System.out.println(조류객체 instanceof 조류);
+
+        System.out.println(참새객체 instanceof 동물);
+        System.out.println(참새객체 instanceof 조류);
+        System.out.println(참새객체 instanceof 참새);
+        System.out.println("==================");
+
+        참새 참새test = new 참새();
+        참새test.test();
+        System.out.println("==================");
+
+        참새test.method();
     }
 
 }
@@ -42,5 +65,39 @@ class Penguin extends Animal {
 
     public void showName(String name) {
         System.out.println("name : " + name);
+    }
+}
+
+abstract class Plant {
+    abstract void photosynthesize();
+}
+
+class 동물 {
+    void method() {
+        System.out.println("동물");
+    }
+}
+
+class 조류 extends 동물 {
+    void method() {
+        super.method();
+        System.out.println("조류");
+    }
+}
+
+class 참새 extends 조류 {
+
+    int a = 10;
+
+    void test() {
+       int a = 20;
+
+       System.out.println(a);
+       System.out.println(this.a);
+    }
+
+    void method() {
+        super.method();
+        System.out.println("참새");
     }
 }
