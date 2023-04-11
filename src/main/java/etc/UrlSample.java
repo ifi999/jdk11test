90015b2d07fbcd8f5ea638e2705d9e6e50bbdc90
ifi999 <kkwkjm4231@gmail.com>
2023-04-11 22:41:59 +0900

    label 및 i++ 증감연산

---
+++ src/main/java/etc/UrlSample.java
@@ -0,0 +1,13 @@
+package etc;
+
+public class UrlSample {
+
+    public static void main(String[] args) {
+        http://www.google.com
+        for(int i=1; i<5;) {
+            if (i++==3) break http;
+            System.out.println(i);
+        }
+    }
+
+}
